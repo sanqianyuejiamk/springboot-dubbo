8fefb696928be96e14786e9aa1d3fe4b7c10964f
hyy044101331 <hyy044101331@163.com>
2017-08-24 21:38:55 +0800

    dubbo服务提供者

---
+++ springboot-dubbo-producer/src/main/java/com/mengka/springboot/service/WechatServiceImpl.java
@@ -0,0 +1,24 @@
+package com.mengka.springboot.service;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.mengka.cloud.service.WechatService;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author mengka
+ * @date 2017/08/24.
+ */
+@Slf4j
+@Service(version = "1.0.0")
+public class WechatServiceImpl implements WechatService {
+
+    /**
+     *  发送微信消息
+     *
+     * @param userId
+     * @param content
+     */
+    public void sendMessage(String userId,String content) {
+        log.info("WechatService sendMessage content = {}",content);
+    }
+}
